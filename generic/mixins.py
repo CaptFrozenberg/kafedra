433fc836c497d828d61d1b86d7dad5e2b27cf3f5
Dmitry <Captfrozenberg@gmail.com>
2016-07-07 17:24:13 +0300

    new main page

--- generic/mixins.py
+++ generic/mixins.py
@@ -5,5 +5,4 @@ class StepsListMixin(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super(StepsListMixin, self).get_context_data(**kwargs)
         context["current_url"] = self.request.path
-        context['doc'] = Document.objects.get(name='Билеты')
         return context
