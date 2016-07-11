2d6aa2adbf35190ab685645c70373c2d8ad07a3b
Dmitry <Captfrozenberg@gmail.com>
2016-07-11 13:07:32 +0300

    fix subject

--- main/views.py
+++ main/views.py
@@ -3,6 +3,27 @@ from django.views.generic.base import TemplateView
 from generic.mixins import SubjectListMixin, DocListMixin
 from main.models import Document, Subject
 
+SUPPORTED_EXTENSIONS = [
+    'doc',
+    'docx',
+    'pdf',
+    'pptx',
+    'rar',
+    'txt',
+    'xls',
+]
+
+def compare(docs, ext):
+    compared = []
+    for doc in docs:
+        extension = doc.file.name.split('.')[-1]
+        if extension in ext:
+            compared.append(('images/{0}.png'.format(extension), doc))
+        else:
+            compared.append(('images/no.png', doc))
+    return compared
+
+
 class MainPageView(TemplateView, SubjectListMixin):
     template_name = 'mainpage.html'
 
@@ -14,20 +35,17 @@ class SubjectView(TemplateView, SubjectListMixin):
         queryset = super(SubjectView, self).get_queryset()
         return queryset
 
-    def get(self, request, *args, **kwargs):
-        try:
-            self.subject = self.kwargs['subject']
-        except:
-            pass
-        return super(SubjectView, self).get(request, *args, **kwargs)
-
     def get_context_data(self, **kwargs):
-        #self.subject_codename = self.request.path.split('/')[-1]
         self.subject_codename = self.kwargs['subject']
         context = super(SubjectView, self).get_context_data(**kwargs)
         subject = Subject.objects.get(code_name=self.subject_codename)
         context['subject_name'] = subject.name
-        context['docs'] = Document.objects.filter(subject__code_name=self.subject_codename)
-        
+        context['docs'] = compare(Document.objects.filter(subject__code_name=self.subject_codename),SUPPORTED_EXTENSIONS)
         print(Document.objects.filter(subject__code_name=self.subject_codename)[0].file.name)
+        return context
+
+class InfoView(TemplateView, SubjectListMixin):
+    def get_context_data(self, **kwargs):
+        self.template_name = self.kwargs['title']
+        context = super(InfoView, self).get_context_data(**kwargs)
         return context
